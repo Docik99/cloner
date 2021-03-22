146095812e26217e99ca95cb25f45c6bb41d3a77
docik <docik@1.ru>
2021-03-22 21:27:03 +0300

    переименован файл и сокращены тесты

--- test_gus.py
+++ test_gus.py
@@ -20,15 +20,3 @@ class ParserTest(unittest.TestCase):
                 self.assertEqual(args.get, 'TestArg')
             else:
                 self.assertEqual(args.token, 'TestArg')
-
-class UserTest(unittest.TestCase):
-    def setUP(self):
-        self.parser = gitlab_user_sync.create_args()
-
-
-    def test_users(self):
-
-        args = self.parser.parse_args(['--token', 'TestArg2'])
-        self.response = gitlab_user_sync.get_user(args)
-        self.response = open("user_data.json", 'r')
-        self.assertEqual(6, self.response.other_web_url)

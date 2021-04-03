e4d03c777c7ed34fb414184079f89e4af780ed4b
docik <docik@1.ru>
2021-04-03 17:56:59 +0300

    реализовано тестирование части кода, не работающего с сервером

---
+++ unit_test_gus.py
@@ -0,0 +1,30 @@
+"""
+tests for gitlab_user_sync
+
+тестирование части НЕ взаимодействующей с сервером
+"""
+
+import unittest
+import gitlab_user_sync
+
+
+class ParserTest(unittest.TestCase):
+    """Класс тестов для работы с parser"""
+
+    def setUp(self):
+        self.parser = gitlab_user_sync.create_args()
+
+    def test_arg(self):
+        """Проверка корректности чтения аргументов"""
+        flag_list = ['--get', '-g', '--token', '-t']
+        for i in range(4):
+            args = self.parser.parse_args([flag_list[i], 'TestArg' + str(i)])
+            if i < 2:
+                self.assertEqual(args.get, 'TestArg' + str(i))
+            else:
+                self.assertEqual(args.token, 'TestArg' + str(i))
+
+    def test_pass(self):
+        """Проверка генерации пароля заданной длины"""
+        pas = gitlab_user_sync.generate_pass(5)
+        self.assertEqual(5, len(pas))
